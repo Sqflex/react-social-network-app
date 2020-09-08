77e602e6f68d0e8537bb6939b57ac0956fdfb271
Sqflex <32853595+Sqflex@users.noreply.github.com>
2020-09-08 16:36:22 +0300

    Add: Dialogs component and css-module

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div>
+            <h2>Dialogs</h2>
+        </div>
+    )
+}
+
+export default Dialogs;
