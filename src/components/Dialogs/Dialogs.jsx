ed01e734e15b00afc112ee4c4c4d83eca6c76953
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-09-09 12:29:51 +0300

    add: html structure for dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,23 @@ import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <h2>Dialogs</h2>
-        </div>
+        <section className={style.dialogs}>
+            <div className={style.dialogs_items}>
+            <div className={style.dialogs_logo}>
+                <h2>Dialogs</h2>
+            </div>
+                <div className={style.dialog + ' ' + style.activeDialog}>Vlad</div>
+                <div className={style.dialog}>Vadim</div>
+                <div className={style.dialog}>Evgen</div>
+                <div className={style.dialog}>Valentin</div>
+            </div>
+            <div className={style.messages}>
+               <div className={style.message}>Hi</div>
+               <div className={style.message}>How are u?</div>
+               <div className={style.message}>Fine, thx. u?</div> 
+            </div>
+        </section>
+
     )
 }
 
