4651c28ad8b758efc70af90c324f2ed85625ce9a
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-09-09 12:06:44 +0300

    fix: import and img alt text

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ import s from './Post.module.css';
 const Post = (props) => {
     return (
           <div className={s.posts__item}>
-            <img src="https://sun9-30.userapi.com/c639724/v639724018/5406b/mqjbhHdkiN8.jpg"></img>
+            <img src="https://sun9-30.userapi.com/c639724/v639724018/5406b/mqjbhHdkiN8.jpg" alt="post"></img>
             {props.message}
             <div>
             {props.likenumber}
