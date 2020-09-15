3c3535110066f7a5c8c6fab2f6f6df3db576e756
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-09-15 03:12:52 +0300

    upd: first use of VD on ref option (bad practise)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,6 +6,13 @@ const MyPosts = (props) => {
 
   let postsElements = props.posts.map (post => <Post message={post.postmessage} likesnumber={post.likes} />)
 
+  let newPostAdd = React.createRef();
+
+  let addPost =()=>{
+    let text = newPostAdd.current.value;
+    alert(text);
+  }
+
   return (
     <div>
       <div className="add-post">
@@ -15,10 +22,9 @@ const MyPosts = (props) => {
       </div>
       <div className={s.postBlock}>
         <h3>My Posts</h3>
-        <div><textarea></textarea></div>
+        <div><textarea ref={newPostAdd}></textarea></div>
         <div>
-          <button>Add Post</button>
-          <button>Remove Post</button>
+          <button onClick={ addPost }>Add Post</button>
         </div>
         <div className={s.posts}>
           {postsElements}
